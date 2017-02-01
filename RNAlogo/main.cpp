09a6a784e4ca3f49c52778e4f134e2f3e16bc5a1
Hiroshi Miyake <miyake@outlook.in>
2017-02-02 00:34:08 +0900

    changed sample case, upload the svg of it

--- RNAlogo/main.cpp
+++ RNAlogo/main.cpp
@@ -14,23 +14,22 @@ using namespace iyak;
 
 int main(int argc, char* argv[]) {
   RNAlogo logo;
-  
+
   logo.set_title("sample");
-  logo.set_x_axis_height(0); /* no position */
-  
+
   logo.map_color("A", "green");
   logo.map_color("G", "gold");
   logo.map_color("C", "royalblue");
   logo.map_color("U", "crimson");
   logo.map_color("Ψ", "#9932CC");
   logo.map_color("Ψo", "#9932CC");
-  
+
   logo.pict_table_bit
   (
-   {{10,22,4,93},{1,24,29, 3},{12,1,19,39},{10,1,91,2}},
-   {{"A","C","G","Ψ"},{"A","C","G","U"},{"A","C","G","Ψo"},{"A","C","G","U"}},
-   {"", "↑", "", ""}
+   {{1,22,4,93},{1,24,29, 3},{12,1,19,39},{5,1,51,2},{23,1,66,3},{56,2,4,6}},
+   {{"A","C","G","Ψ"},{"A","C","G","U"},{"A","C","G","Ψo"},{"A","C","G","U"},
+    {"A","C","G","U"},{"A","C","G","U"}}, {}
    );
-  
+
   return 0;
 }
